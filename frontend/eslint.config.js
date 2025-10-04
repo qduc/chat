3650b1428df2c8dd548626771e86f0352852d44d
qduc <qduc159@gmail.com>
2025-10-04 16:04:26 +0700

    fix: update ESLint configuration to include global ignores and refine rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,11 +11,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  globalIgnores(["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]),
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
     rules: {
       '@next/next/no-img-element': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ];
