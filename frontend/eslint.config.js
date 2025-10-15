91629fb6cdeffd1bcb22b1c99fe3695f8eb39e13
Duc Nguyen <qduc159@gmail.com>
2025-10-15 11:03:22 +0700

    feat: add Prettier configuration and integrate with ESLint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
 import { globalIgnores } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,11 +13,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   globalIgnores(["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]),
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
       '@next/next/no-img-element': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': ['error'],
     },
   },
 ];
